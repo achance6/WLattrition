3e2370cc865b228e384ddc7dc4038a6b0a9908e3
achance6 <ayden.chance@rocketmail.com>
2020-04-24 00:26:23 -0400

    Baseline functionality implemented

--- Server_AdvanceTurn.lua
+++ Server_AdvanceTurn.lua
@@ -1,30 +1,32 @@
 
 function Server_AdvanceTurn_Start(game,addOrder)
-    local standing = game.ServerGame.LatestTurnStanding;
-	local default_areas = game.MapDetails.Bonuses
-	local overridden_areas = game.GameSettings.OverriddenBonuses
+  local standing = game.ServerGame.LatestTurnStanding;
+	local default_areas = game.Map.Bonuses
+	local overridden_areas = game.Settings.OverriddenBonuses
 	local areas = {}
 	
 	--put overridden areas into our areas map
 	for area_id, bonus_details in pairs(default_areas) do
+		areas[area_id] = default_areas[area_id]
 		if overridden_areas[area_id] ~= nil then
 			areas[area_id] = overridden_areas[area_id]
 		end
 	end
 	
 	attritionOrder = {}
-	local cur = 0
+  cur = 1
 	for area_id, bonus_details in pairs(areas) do
 		local total_armies = 0
-		for curr_ter_ID in bonus_details.Territories do
-			total_armies = total_armies + standing[curr_ter_ID].NumArmies.NumArmies
+		for _, curr_ter_ID in pairs(bonus_details.Territories) do
+			total_armies = total_armies + standing.Territories[curr_ter_ID].NumArmies.NumArmies
 		end
 		if total_armies > Mod.PublicGameData.supply_limit_data[area_id] then
-			for _, terr in pairs(standing.Territories)
+			for _, curr_ter_ID in pairs(bonus_details.Territories) do
 			terrMod = WL.TerritoryModification.Create(curr_ter_ID);
-			terrMod.SetArmiesTo = terr.NumArmies.NumArmies * 0.8; --TODO: Variable attrition
+			terrMod.SetArmiesTo = standing.Territories[curr_ter_ID].NumArmies.NumArmies * 0.8; --TODO: Variable attrition
 			attritionOrder[cur] = terrMod
 			cur = cur + 1
+			end
 		end
 	end
 	addOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, 'Attrition', nil, attritionOrder));
--- Server_StartGame.lua
+++ Server_StartGame.lua
@@ -1,11 +1,12 @@
 function Server_StartGame(game, standing) 
-	local def_bonuses = game.MapDetails.Bonuses
-	local overriden_bonuses = game.GameSettings.OverriddenBonuses
+	print("test")
+	local def_bonuses = game.Map.Bonuses
+	local overriden_bonuses = game.Settings.OverriddenBonuses
 	local public_game_data = Mod.PublicGameData
 	local supply_limit_mult = 15
 	public_game_data.supply_limit_data = {}
-	for area_id, bonus in pairs(def_bonuses) do
-		public_game_data.supply_limit_data[area_id] = bonus * supply_limit_mult
+	for area_id, bonus_details in pairs(def_bonuses) do
+		public_game_data.supply_limit_data[area_id] = bonus_details.Amount * supply_limit_mult
 	end
 	for area_id, bonus_details in pairs(overriden_bonuses) do
 		public_game_data.supply_limit_data[area_id] = bonus_details.Amount * supply_limit_mult
