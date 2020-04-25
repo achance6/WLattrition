e8bc886b9c1d1294435356011dbb384127266420
achance6 <ayden.chance@rocketmail.com>
2020-04-24 21:20:20 -0400

    put hard cap on bonuses

--- Server_AdvanceTurn.lua
+++ Server_AdvanceTurn.lua
@@ -16,26 +16,28 @@ function Server_AdvanceTurn_Start(game,addOrder)
 	attritionOrder = {}
   cur = 1
 	for bonusID, bonusDetails in pairs(bonuses) do
-		local totalArmies = 0
-    --TODO: don't count neutral armies
-    playerOwners = {}
-    local count = 1
-		for _, currTerID in pairs(bonusDetails.Territories) do
-      if not isInWhiteList(standing.Territories[currTerID].OwnerPlayerID) and not standing.Territories[currTerID].IsNeutral then
-        totalArmies = totalArmies + standing.Territories[currTerID].NumArmies.NumArmies
-      end
-      playerOwners[count] = standing.Territories[currTerID].OwnerPlayerID
-		end
-		if totalArmies > Mod.PublicGameData.supplyLimitData[bonusID] then
-			for _, currTerID in pairs(bonusDetails.Territories) do
+    if bonusDetails.Amount < 13 then
+      local totalArmies = 0
+      --TODO: don't count neutral armies
+      playerOwners = {}
+      local count = 1
+      for _, currTerID in pairs(bonusDetails.Territories) do
         if not isInWhiteList(standing.Territories[currTerID].OwnerPlayerID) and not standing.Territories[currTerID].IsNeutral then
-          terrMod = WL.TerritoryModification.Create(currTerID);
-          terrMod.SetArmiesTo = standing.Territories[currTerID].NumArmies.NumArmies * 0.8; --TODO: Variable attrition
-          attritionOrder[cur] = terrMod
-          cur = cur + 1
+          totalArmies = totalArmies + standing.Territories[currTerID].NumArmies.NumArmies
         end
-			end
-		end
+        playerOwners[count] = standing.Territories[currTerID].OwnerPlayerID
+      end
+      if totalArmies > Mod.PublicGameData.supplyLimitData[bonusID] then
+        for _, currTerID in pairs(bonusDetails.Territories) do
+          if not isInWhiteList(standing.Territories[currTerID].OwnerPlayerID) and not standing.Territories[currTerID].IsNeutral then
+            terrMod = WL.TerritoryModification.Create(currTerID);
+            terrMod.SetArmiesTo = standing.Territories[currTerID].NumArmies.NumArmies * 0.8; --TODO: Variable attrition
+            attritionOrder[cur] = terrMod
+            cur = cur + 1
+          end
+        end
+      end
+    end
 	end
 	addOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, 'Attrition', nil, attritionOrder));
 end
@@ -47,4 +49,22 @@ function isInWhiteList(playerID)
     end
   end
   return false
+end
+
+function Server_AdvanceTurn_Order(game, gameOrder, gameOrderResult, skipThisOrder, addNewOrder) 
+  eatOrder = {}
+  cur = 1
+  if isInWhiteList(gameOrder.PlayerID) and type(gameOrderResult) == "GameOrderAttackTransferResult"  then
+    local defendingArmiesKilled = gameOrderResult.GameOrderAttackTransferResult.DefendingArmiesKilled
+      --if gameOrderResult.GameOrderAttackTransferResult.IsSuccessful then
+        --local armiesRemaining = gameOrder.GameOrderResult.GameOrderAttackTransferResult.ActualArmies - gameOrder.GameOrderResult.GameOrderAttackTransferResult.AttackingArmiesKilled
+        terrMod = WL.TerritoryModification.Create(gameOrder.To)
+        --terrMod.SetArmiesTo = armiesRemaining + (0.2 * defendingArmiesKilled)
+        terrMod.SetArmiesTo = 30
+        eatOrder[cur] = terrMod
+        cur = cur + 1
+      --end
+    addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, 'Eat', nil, eatOrder))
+  end
+  
 end
